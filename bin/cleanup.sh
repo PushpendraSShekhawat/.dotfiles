04273628e8d2210d8ba7a016feb30b884dfa1034
PushpendraSShekhawat <111778003+PushpendraSShekhawat@users.noreply.github.com>
2022-11-18 12:54:26 -0500

    Update cleanup.sh

--- bin/cleanup.sh
+++ bin/cleanup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-rm -f ~/.vimrc
-EMPTY=""
+rm -f ~/.vimrc                          #Removes the .vimrc file from the home directory forcibly.
+EMPTY=""                                
 VAR="source ~/.dotfiles/bashrc_custom"
-sed -i "s|$VAR|$EMPTY|p" ~/.bashrc
+sed -i "s|$VAR|$EMPTY|p" ~/.bashrc      #Removes lines containing 'source ~/.dotfiles/bashrc_custom' from .bashrc located in the home directory.
 
-rm -r ~/.TRASH
+rm -r ~/.TRASH                          #Removes a directory called .TRASH from the home directory.
