8c5604f2a28b3db69ef7c7500b4eb841e3d8c3d9
Pushpendra Shekhawat <pushpendra.shekhawat96@gmail.com>
2022-11-21 22:14:15 -0500

    Created linux shell setup scripts.

--- bin/cleanup.sh
+++ bin/cleanup.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
-<<<<<<< HEAD
-rm -f ~/.vimrc
-E=""
-VAR="source ~/.dotfiles/bashrc_custom"
-sed -i "s|source ~/.dotfiles/bashrc_custom| |g" ~/.bashrc
-=======
-rm -f ~/.vimrc                          #Removes the .vimrc file from the home directory forcibly.
+rm -f ~/.vimrc                          #Removes the .vimrc file from the home directory forcibly
 EMPTY=""                                
 VAR="source ~/.dotfiles/bashrc_custom"
 sed -i "s|$VAR|$EMPTY|p" ~/.bashrc      #Removes lines containing 'source ~/.dotfiles/bashrc_custom' from .bashrc located in the home directory.
->>>>>>> ce552c8f2ff226e49121fd9f058bd11974705e40
 
 rm -r ~/.TRASH                          #Removes a directory called .TRASH from the home directory.
